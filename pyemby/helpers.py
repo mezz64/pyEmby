4f201c468cc507f28cad17985ae847ddd4ff5bfb
Matt <mcarlton00@gmail.com>
2020-10-01 22:17:01 -0400

    Clean server payloads to remove None values

--- pyemby/helpers.py
+++ pyemby/helpers.py
@@ -5,6 +5,7 @@ Function helpers.
 Copyright (c) 2017-2019 John Mihalic <https://github.com/mezz64>
 Licensed under the MIT license.
 """
+import collections.abc
 
 
 def deprecated_name(name):
@@ -20,3 +21,44 @@ def deprecated_name(name):
                 return func(self)
         return func_wrapper
     return decorator
+
+
+def clean_none_dict_values(obj):
+    """
+    Recursively remove keys with a value of None
+    """
+    if not isinstance(obj, collections.abc.Iterable) or isinstance(obj, str):
+        return obj
+
+    queue = [obj]
+
+    while queue:
+        item = queue.pop()
+
+        if isinstance(item, collections.abc.Mapping):
+            mutable = isinstance(item, collections.abc.MutableMapping)
+            remove = []
+
+            for key, value in item.items():
+                if value is None and mutable:
+                    remove.append(key)
+
+                elif isinstance(value, str):
+                    continue
+
+                elif isinstance(value, collections.abc.Iterable):
+                    queue.append(value)
+
+            if mutable:
+                # Remove keys with None value
+                for key in remove:
+                    item.pop(key)
+
+        elif isinstance(item, collections.abc.Iterable):
+            for value in item:
+                if value is None or isinstance(value, str):
+                    continue
+                elif isinstance(value, collections.abc.Iterable):
+                    queue.append(value)
+
+    return obj
